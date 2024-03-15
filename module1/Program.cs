8113feda1cde1668fe13ad8b666b4cb1329df9dd
Лихва Максим <maksym.lykhva@lnu.edu.ua>
2024-03-15 09:27:08 +0200

    Add project files.

---
+++ module1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix();
+            matrix.FillMatrixRandom(3,5,0,8);
+        }
+    }
+
+    class Matrix
+    {
+        int[][] matrix;
+
+        public void FillMatrixRandom(int rows, int columns, int min, int max)
+        {
+            var random = new Random();
+            matrix = new int[rows][columns];
+
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = 0; j < columns; ++j)
+                {
+                    matrix[i][j] = random.Next(min, max);
+                }
+            }
+        }
+        
+        public int this[int r, int c]
+        {
+            get { return matrix[r, c]; }
+        }
+    }
+}
